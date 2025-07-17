d7338e5c6dfde6f8685af3579b6ae42dfd7c567c
roberthoust <robertallenhoust@gmail.com>
2025-07-17 07:17:35 -0600

    Initial Commit

---
+++ CloudTune/CloudTuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudTuneApp.swift
+//  CloudTune
+//
+//  Created by Robert Houst on 7/17/25.
+//
+
+import SwiftUI
+
+@main
+struct CloudTuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CloudTuneTests/CloudTuneTests.swift
@@ -0,0 +1,17 @@
+//
+//  CloudTuneTests.swift
+//  CloudTuneTests
+//
+//  Created by Robert Houst on 7/17/25.
+//
+
+import Testing
+@testable import CloudTune
+
+struct CloudTuneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
